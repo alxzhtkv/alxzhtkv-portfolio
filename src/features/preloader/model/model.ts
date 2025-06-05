61b1eb61e92953e099383224a93fb411ebb636fd
alxzhtkv <alxzhtkv@gmail.com>
2025-06-05 12:52:31 +0300

    FIX

--- src/features/preloader/model/model.ts
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-import { PreloaderStore } from "./types";
-
-export const usePreloaderStore = create<PreloaderStore>((set) => ({
-    show: true,
-    setShow: (show) => set(() => ({ show }))
-}));
--- src/features/preloader/model/types.ts
+++
@@ -1,4 +0,0 @@
-export type PreloaderStore = {
-    show: boolean;
-    setShow: (show: boolean) => void;
-};
